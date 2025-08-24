e4edde57cb00bf4a79906e04c1eb6cb75c3576ec
asshissh <ahishsingh27082002@gmail.com>
2025-08-25 01:28:45 +0530

    feat: add user profile update, password change, and watch history retrieval routes; refactor user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,19 @@
 
-import {Router} from 'express';
-import { loginUser, logoutUser, registerUser } from '../controllers/user.controllers.js';
+import { Router } from 'express';
+import {
+    changeCurrentPassword,
+    getCurrentUser,
+    getUserChannelProfile,
+    getWatchHistory,
+    loginUser,
+    logoutUser,
+    registerUser,
+    upadateAccountDetails,
+    updateUserAvatar
+} from '../controllers/user.controllers.js';
 import { upload } from '../middlewares/multer.middlewares.js';
 import { verifyJWT } from '../middlewares/auth.middlewares.js';
+import { get } from 'mongoose';
 const router = Router();
 
 router.route("/register").post(
@@ -16,7 +27,14 @@ router.route("/register").post(
 router.route("/login").post(loginUser)
 
 //secure route
-router.route("/logout").post(verifyJWT,logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/upadate-profile").patch(verifyJWT, upadateAccountDetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-photo").patch(verifyJWT, upload.single("coverPhoto"), updateUserAvatar)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 
 
